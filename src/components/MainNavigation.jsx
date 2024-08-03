f376161204d4fff5e5da63a643b08d9524deca05
Dylanc55 <dylan.cooper55@gmail.com>
2024-08-03 12:18:05 +0900

    created MainNavigation component

---
+++ src/components/MainNavigation.jsx
@@ -0,0 +1,33 @@
+import { NavLink } from "react-router-dom";
+
+const MainNavigation = () => {
+  return (
+    <header>
+      <nav>
+        <h3>Pokemon App</h3>
+
+        <ul>
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) => (isActive ? "active" : undefined)}
+            >
+              Home
+            </NavLink>
+          </li>
+
+          <li>
+            <NavLink
+              to="/pokemon"
+              className={({ isActive }) => (isActive ? "active" : undefined)}
+            >
+              Pokemon
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default MainNavigation;
