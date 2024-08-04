45fe89da603ec964a5e24ab0af967ae36685d5bd
Dylanc55 <dylan.cooper55@gmail.com>
2024-08-04 17:53:54 +0900

    basic move detail page

--- src/pages/MoveDetail.jsx
+++ src/pages/MoveDetail.jsx
@@ -1,14 +1,86 @@
 import { Suspense } from "react";
-import { Await, useLoaderData } from "react-router-dom";
+import { Await, Link, useLoaderData } from "react-router-dom";
+
+import Card from "../components/UI/Card";
+import Grid from "../components/UI/Grid";
+
+import classes from "./MoveDetail.module.css";
 
 const MoveDetailPage = () => {
   const { move } = useLoaderData();
   return (
     <Suspense fallback={<p style={{ textAlign: "center" }}>Loading...</p>}>
       <Await resolve={move}>
-        {(loadedMove) => {
-          console.log("loadedMove :>> ", loadedMove);
-          return <div>Move Detail Page</div>;
+        {({
+          name,
+          accuracy,
+          power,
+          pp,
+          damage_class,
+          effect_chance,
+          effect_entries,
+          learned_by_pokemon,
+        }) => {
+          const entry = effect_entries.find(
+            (entry) => entry.language.name === "en"
+          );
+
+          return (
+            <section className={classes.move}>
+              <h1>{name.toUpperCase()}</h1>
+
+              <Card>
+                <p className={classes.description}>{entry.effect}</p>
+              </Card>
+
+              <h3>Stats</h3>
+
+              <Card>
+                <Grid>
+                  {!!accuracy && (
+                    <li>
+                      <h4 className={classes.title}>accuracy</h4>
+                      <p className={classes["content-lg"]}>{accuracy}</p>
+                    </li>
+                  )}
+                  {!!power && (
+                    <li>
+                      <h4 className={classes.title}>power</h4>
+                      <p className={classes["content-lg"]}>{power}</p>
+                    </li>
+                  )}
+                  <li>
+                    <h4 className={classes.title}>power points</h4>
+                    <p className={classes["content-lg"]}>{pp}</p>
+                  </li>
+                  <li>
+                    <h4 className={classes.title}>damage class</h4>
+                    <p className={classes["content-lg"]}>{damage_class.name}</p>
+                  </li>
+                  {effect_chance && (
+                    <li>
+                      <h4 className={classes.title}>effect chance</h4>
+                      <p className={classes["content-lg"]}>{effect_chance}</p>
+                    </li>
+                  )}
+                </Grid>
+              </Card>
+
+              <h3>Pokémon</h3>
+
+              <Card>
+                <Grid>
+                  {learned_by_pokemon.map((pokemon) => (
+                    <li key={pokemon.name}>
+                      <Link to={`/pokemon/${pokemon.name}`}>
+                        <p className={classes.content}>{pokemon.name}</p>
+                      </Link>
+                    </li>
+                  ))}
+                </Grid>
+              </Card>
+            </section>
+          );
         }}
       </Await>
     </Suspense>
